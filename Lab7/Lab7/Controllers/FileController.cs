08fb7c3b89c832024b5e6c237d31c0139cf08fe6
Rew1nd ⠀ <bogdin200056@gmail.com>
2024-10-11 13:09:57 +0300

    Lab7

---
+++ Lab7/Lab7/Controllers/FileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace Lab7.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+        {
+            string fileContent = $"Ім'я: {firstName}\nПрізвище: {lastName}";
+
+            byte[] fileBytes = Encoding.UTF8.GetBytes(fileContent);
+            return File(fileBytes, "text/plain", $"{fileName}.txt");
+        }
+    }
+}
