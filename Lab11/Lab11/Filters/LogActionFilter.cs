fd2649b014716172cab43a28caf73530764100e8
Rew1ndy <bogdin200056@gmail.com>
2024-10-20 09:57:36 +0300

    Lab11

---
+++ Lab11/Lab11/Filters/LogActionFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Lab11.Filters
+{
+    public class LogActionFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // Логіка, яка виконується перед виконанням методу дії
+            var actionName = context.ActionDescriptor.DisplayName;
+            var currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+            // Запис до файлу
+            File.AppendAllText("log.txt", $"Action: {actionName}, Time: {currentTime}\n");
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            //
+        }
+    }
+}
---
+++ Lab11/Lab11/Filters/UniqueUserFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Lab11.Filters
+{
+    public class UniqueUserFilter : IActionFilter
+    {
+        private static HashSet<string> uniqueUsers = new HashSet<string>();
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // Отримання IP-адреси користувача
+            var userIp = context.HttpContext.Connection.RemoteIpAddress?.ToString();
+
+            if (!uniqueUsers.Contains(userIp))
+            {
+                uniqueUsers.Add(userIp);
+                File.AppendAllText("userCount.txt", $"New user: {userIp}, Total users: {uniqueUsers.Count}\n");
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            //
+        }
+    }
+}
